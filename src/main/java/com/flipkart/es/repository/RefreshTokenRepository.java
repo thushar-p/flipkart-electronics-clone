c4f6221003f6c072f5cb5aa5d4b1eb353145937e
thushar <thusharkulal8@gmail.com>
2024-02-12 15:02:25 +0530

    feature/ implemented schedule jobs to delete expired tokens

--- src/main/java/com/flipkart/es/repository/RefreshTokenRepository.java
+++ src/main/java/com/flipkart/es/repository/RefreshTokenRepository.java
@@ -1,5 +1,7 @@
 package com.flipkart.es.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,7 @@ import com.flipkart.es.entity.RefreshToken;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
 
 	Optional<RefreshToken> findByRefreshToken(String rt);
+
+	List<RefreshToken> findByRefreshTokenExpirationTimeBefore(LocalDateTime now);
     
 }
